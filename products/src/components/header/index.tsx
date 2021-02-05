47ceeb8771b38e6eb3841cc8cd5d7c3c63490040
EduardoAraD <araujocarlos893@gmail.com>
2021-02-05 10:23:37 -0300

    Create component Header

---
+++ products/src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './header.css'
+
+export default function HeaderComponent() {
+    return (
+        <header id="header-app">
+            <h3>TECprodutos</h3>
+        </header>
+    )
+}
