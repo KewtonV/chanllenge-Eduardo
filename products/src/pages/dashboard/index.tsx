29210cb0d450a926f0011d67cc36465ac5372553
EduardoAraD <araujocarlos893@gmail.com>
2021-02-05 12:02:38 -0300

    Update Dashboard

--- products/src/pages/dashboard/index.tsx
+++ products/src/pages/dashboard/index.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Product } from '../../models/product'
 
-export default function dashboard() {
+import './dashboard.css'
+
+const data:Product[] = [
+    {
+        id: 0,
+        name: 'Notebook LG',
+        price: 2299.99,
+        quantidad: 2
+    },
+    {
+        id: 1,
+        name: 'Notebook LG',
+        price: 2299.99,
+        quantidad: 2
+    },
+    {
+        id: 2,
+        name: 'Notebook LG',
+        price: 2299.99,
+        quantidad: 2
+    },
+    {
+        id: 3,
+        name: 'Notebook LG',
+        price: 2299.99,
+        quantidad: 2
+    }
+]
+
+export default function Dashboard() {
+    const [filter, setFilter] = useState('')
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div id="dashboard">
+            <h1>Lista de Produtos Tec</h1>
+            <div className="actions">
+                <input value={filter} onChange={(event) => setFilter(event.target.value) } />
+                <Link to="/adicionar">Adicionar Produto</Link>
+            </div>
+            <table>
+                <tr>
+                    <th>Produtos</th>
+                    <th>Preço</th>
+                    <th>Quantidade</th>
+                    <th>Ações</th>
+                </tr>
+                { data.map((item) => {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                            <td>{item.quantidad}</td>
+                            <td>Test</td>
+                        </tr>
+                    )
+                })}
+            </table>
         </div>
     )
 } 
