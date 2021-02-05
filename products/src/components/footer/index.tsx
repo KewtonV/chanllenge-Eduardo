65e444e38a6f9eede12bb6fba79b40cd3596e234
EduardoAraD <araujocarlos893@gmail.com>
2021-02-05 10:24:01 -0300

    Create component Footer

---
+++ products/src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './footer.css'
+
+export default function FooterComponent() {
+    return (
+        <footer id="footer-app">
+            <span>@Copyright Servac - Chanllenge Eduardo</span>
+        </footer>
+    )
+}
