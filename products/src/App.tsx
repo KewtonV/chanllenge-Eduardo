7a3392514c19a4054683303dc8112abf47d0cf1b
EduardoAraD <araujocarlos893@gmail.com>
2021-02-05 11:18:13 -0300

    Add css and components in App.tsx

--- products/src/App.tsx
+++ products/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+
+import HeaderComponent from './components/header'
+import FooterComponent from './components/footer';
 import Dashboard from './pages/dashboard'
+
+import './app.css'
 
 function App() {
   return (
-    <Dashboard />
+    <div id="app">
+      <HeaderComponent />
+      <Dashboard />
+      <FooterComponent />
+    </div>
   );
 }
 
