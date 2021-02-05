ef5de8b2d751c657f3b789fdd96de172bd4ce5c9
EduardoAraD <araujocarlos893@gmail.com>
2021-02-05 12:00:42 -0300

    Create page addProducts

---
+++ products/src/pages/addProduct/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './addProduct.css'
+
+export default function AddProduct() {
+    return (
+        <div id="add-product">
+            <h1>Adicionar Produto</h1>
+        </div>
+    )
+}
