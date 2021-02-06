aeeba59267b44e74ca3651df1e92dfed755aad3d
EduardoAraD <araujocarlos893@gmail.com>
2021-02-06 17:20:55 -0300

    Update dashboard

--- products/src/pages/dashboard/index.tsx
+++ products/src/pages/dashboard/index.tsx
@@ -1,63 +1,53 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Product } from '../../models/product'
+import { clickUpdateProduct, removeProduct } from '../../store/products/action'
+
+import { ProductsState } from '../../store/products/reducers'
 
 import './dashboard.css'
 
-const data:Product[] = [
-    {
-        id: 0,
-        name: 'Notebook LG',
-        price: 2299.99,
-        quantidad: 2
-    },
-    {
-        id: 1,
-        name: 'Notebook LG',
-        price: 2299.99,
-        quantidad: 2
-    },
-    {
-        id: 2,
-        name: 'Notebook LG',
-        price: 2299.99,
-        quantidad: 2
-    },
-    {
-        id: 3,
-        name: 'Notebook LG',
-        price: 2299.99,
-        quantidad: 2
-    }
-]
-
 export default function Dashboard() {
-    const [filter, setFilter] = useState('')
+    const dispatch = useDispatch()
+    const products = useSelector<ProductsState, ProductsState["products"]>((state) => state.products)
+
+    function handleClickUpdate(product: Product) {
+        console.log(product)
+        dispatch(clickUpdateProduct(product))
+    }
+
+    function handleClickRemove(product: Product){
+        console.log(product)
+        dispatch(removeProduct(product))
+    }
 
     return (
         <div id="dashboard">
             <h1>Lista de Produtos Tec</h1>
-            <div className="actions">
-                <input value={filter} onChange={(event) => setFilter(event.target.value) } />
-                <Link to="/adicionar">Adicionar Produto</Link>
-            </div>
             <table>
+                <thead>
                 <tr>
                     <th>Produtos</th>
                     <th>Preço</th>
                     <th>Quantidade</th>
                     <th>Ações</th>
                 </tr>
-                { data.map((item) => {
+                </thead>
+                <tbody>
+                { products.map(item => {
                     return (
                         <tr key={item.id}>
                             <td>{item.name}</td>
                             <td>{item.price}</td>
                             <td>{item.quantidad}</td>
-                            <td>Test</td>
+                            <td>
+                                <button onClick={() => handleClickUpdate(item)}>Atualizar</button>
+                                <button onClick={() => handleClickRemove(item)}>Delete</button>
+                            </td>
                         </tr>
                     )
                 })}
+                </tbody>
             </table>
         </div>
     )
